3556652b576b636d2f78eea687853e8f67012ea9
Maciej Adamiak <adamiak.maciek@gmail.com>
2018-05-10 19:50:06 +0200

    enhanced rate limit object with additional headers related to streams

---
+++ src/test/scala/com/madamiak/twitch/model/RateLimitTest.scala
@@ -0,0 +1,50 @@
+package com.madamiak.twitch.model
+
+import akka.http.scaladsl.model.HttpResponse
+import akka.http.scaladsl.model.headers.RawHeader
+import org.scalatest.{ Matchers, WordSpec }
+
+class RateLimitTest extends WordSpec with Matchers {
+
+  "rate limit" when {
+
+    "creating from HTTP response" should {
+
+      "be successful when all rate limit data is available" in {
+        val httpResponse: HttpResponse = HttpResponse()
+          .addHeader(RawHeader("additional-header", "999"))
+          .addHeader(RawHeader("", ""))
+          .addHeader(RawHeader("ratelimit-limit", "1"))
+          .addHeader(RawHeader("ratelimit-remaining", "2"))
+          .addHeader(RawHeader("ratelimit-reset", "3"))
+
+        RateLimit(httpResponse) shouldEqual RateLimit(1, 2, 3)
+      }
+
+      "be successful when all rate limit extended data is available" in {
+        val httpResponse: HttpResponse = HttpResponse()
+          .addHeader(RawHeader("additional-header", "999"))
+          .addHeader(RawHeader("", ""))
+          .addHeader(RawHeader("ratelimit-limit", "1"))
+          .addHeader(RawHeader("ratelimit-remaining", "2"))
+          .addHeader(RawHeader("ratelimit-reset", "3"))
+          .addHeader(RawHeader("ratelimit-helixstreamsmetadata-limit", "4"))
+          .addHeader(RawHeader("ratelimit-helixstreamsmetadata-remaining", "5"))
+
+        RateLimit(httpResponse) shouldEqual RateLimit(1, 2, 3, Some(4), Some(5))
+      }
+
+      "should fail when rate limit data is not valid" in {
+        val httpResponse: HttpResponse = HttpResponse()
+          .addHeader(RawHeader("ratelimit-limit", "-100"))
+          .addHeader(RawHeader("ratelimit-remaining", "2"))
+          .addHeader(RawHeader("ratelimit-reset", "3"))
+
+        intercept[IllegalArgumentException](RateLimit(httpResponse))
+      }
+
+    }
+
+  }
+
+}
