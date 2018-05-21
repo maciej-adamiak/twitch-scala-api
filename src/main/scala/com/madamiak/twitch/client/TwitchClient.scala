71b7baf41e9d9a27de98633d85a710d41821ef87
Maciej Adamiak <adamiak.maciek@gmail.com>
2018-05-21 20:52:15 +0200

    Avoided configuration multiple loads

--- src/main/scala/com/madamiak/twitch/client/TwitchClient.scala
+++ src/main/scala/com/madamiak/twitch/client/TwitchClient.scala
@@ -2,16 +2,16 @@ package com.madamiak.twitch.client
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.Uri.{Path, Query}
+import akka.http.scaladsl.model.Uri.{ Path, Query }
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.unmarshalling.{Unmarshal, Unmarshaller}
+import akka.http.scaladsl.unmarshalling.{ Unmarshal, Unmarshaller }
 import akka.stream.ActorMaterializer
 import com.madamiak.twitch.client.header.ClientIdHeader
 import com.madamiak.twitch.model._
 import com.madamiak.twitch.model.api.TwitchPayload
-import com.typesafe.config.ConfigFactory.load
+import com.typesafe.config.{ Config, ConfigFactory }
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
 class TwitchClient(
     implicit val system: ActorSystem,
@@ -19,13 +19,15 @@ class TwitchClient(
     implicit val materializer: ActorMaterializer,
 ) {
 
-  require(load().hasPath("twitch.api.scheme"), "Twitch api schema not defined")
-  require(load().hasPath("twitch.api.host"), "Twitch api host not defined")
-  require(load().hasPath("twitch.client.id"), "Twitch api client id not defined")
+  private val config: Config = ConfigFactory.load()
+
+  require(config.hasPath("twitch.api.scheme"), "Twitch api schema not defined")
+  require(config.hasPath("twitch.api.host"), "Twitch api host not defined")
+  require(config.hasPath("twitch.client.id"), "Twitch api client id not defined")
 
   private val twitchUri: Uri = Uri()
-    .withScheme(load().getString("twitch.api.scheme"))
-    .withHost(load().getString("twitch.api.host"))
+    .withScheme(config.getString("twitch.api.scheme"))
+    .withHost(config.getString("twitch.api.host"))
 
   def http[T](
       path: String
@@ -38,7 +40,7 @@ class TwitchClient(
               .withPath(Path(path))
               .withQuery(query)
           )
-          .withHeaders(ClientIdHeader(load().getString("twitch.client.id")))
+          .withHeaders(ClientIdHeader(config.getString("twitch.client.id")))
       )
       .flatMap(extractData[T])
 
